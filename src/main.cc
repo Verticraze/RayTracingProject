d7d86b6e5ebf7d00fb480ee8a3977bad832678ec
Verticraze <verticraze@outlook.com>
2026-04-22 19:07:49 +0530

    Added test for ray

--- src/main.cc
+++ src/main.cc
@@ -1,28 +1,50 @@
-﻿// CMakeProject1.cpp : Defines the entry point for the application.
-//
+﻿#include<iostream>
+#include<ray.h>
 #include"vec3.h"
 #include"color.h"
-#include "rayTracing.h"
 
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
 
-int main()
-{
-	int imageWidth{ 256 };
-	int imageHeight{ 256 };
+int main() {
+	auto aspect_ratio = 4.0/3.0;
 
-	std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+	int image_width = 400;
+	int image_height = int( image_width / aspect_ratio );
+	image_height = (image_height < 1) ? 1 : image_height;
 
-	for (int j = 0; j < imageHeight; j++)
+	auto focal_length = 1.0;
+	auto viewport_height = 2.0;
+	auto viewport_width = viewport_height * (double(image_width)/ image_height);
+	auto center = point3(0,0,0);
+
+	auto viewport_u = vec3(viewport_width,0,0);
+	auto viewport_v = vec3(0,-viewport_height,0);
+
+	auto pixel_delta_u = viewport_u / image_width;
+	auto pixel_delta_v = viewport_v / image_height;
+
+	auto viewport_top_left = center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+	auto origin = viewport_top_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = 0; j < image_height; j++)
 	{
-		std::clog << "\rScan lines remaining: " << (imageHeight - j) << ' ' << std::flush;
-		for (int i = 0; i < imageWidth; i++)
+		std::clog << "\rScan lines remaining: " << (image_height - j) << ' ' << std::flush;
+		for (int i = 0; i < image_width; i++)
 		{
-			auto pixel_color = color(double(i) / (imageWidth - 1), double(j) / (imageHeight - 1), 0);
+			auto pixel_center = origin + (i*pixel_delta_u) + (j * pixel_delta_v);
+			auto ray_dir = pixel_center - center;
+			ray r (center,ray_dir);
+			color pixel_color = ray_color(r);
 			write_color(std::cout,pixel_color);
 		}
 	}
 
 	std::clog << "\r Done.              \n";
 	return 0;
-
 }
